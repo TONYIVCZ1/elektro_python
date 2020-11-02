edb60f9ab9c30c60b9bcc21972e30956460ca7a8
TONYIVCZ1 <67112544+TONYIVCZ1@users.noreply.github.com>
2020-11-02 16:35:51 +0100

    Create VypocetVykonu.py

---
+++ VypocetVykonu.py
@@ -0,0 +1,16 @@
+#Zadání hodnot. Za neznámou dosaď 0. 
+p = float(input("Zadej Výkon ve W: "))
+u = float(input("Zadej napětí ve V: "))
+i = float(input)"Zadej proud v A: "))
+#Zjištění neznámé
+if p == 0 and u == 0 and i == 0: 
+    print("Prosím, zadej 2 čísla jejich hodnota není 0. ")
+elif p == 0 and u == 0 or u == 0 and i == 0 or p == 0 and i == 0:
+    print("Zadej ještě jedno číslo. ")
+else:
+    if p == 0:
+        print(u, " V x ", i, " A= ", u*i, " W. ")
+    if u == 0:
+        print(p, " W : ", i, " A= ", p/i, " V. ")
+    if i == 0:
+        print(p, " W : ", u, " V= ", p/u, " A. ")
